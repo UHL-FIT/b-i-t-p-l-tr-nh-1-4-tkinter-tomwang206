5c9c50a258fab1bb668da3cdc0678a864059719d
tomwang <ducquangahihi@gmail.com>
2026-05-07 11:59:13 +0700

    change sth in UI

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import tkinter as tk
 
 # 1. Khởi tạo cửa sổ gốc
 root = tk.Tk()
-root.title("Ứng dụng đầu tiên - UHL")
-root.geometry("400x200")
+root.title("Trần Đức Quang")
+root.geometry("500x500")
 
 # 2. Tạo thành phần hiển thị văn bản (Label)
 nhan_chao = tk.Label(root, text="Chào mừng sinh viên Đại học Hạ Long!")
